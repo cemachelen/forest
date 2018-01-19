6e1d8a431910739e2e426e1aa647597945f38840
Theo McCaie <tam203@gmail.com>
2018-01-19 14:54:44 +0000

    dont run the apps that cause memmory leak

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ pysssix /s3 >/dev/null 2>&1 &
 
 if [ -z "$PUBLIC_IP" ]
 then
-    bokeh serve --port 8888 /opt/model_evaluation_tool/documents/*
+    bokeh serve --port 8888 /opt/model_evaluation_tool/documents/interaction /opt/model_evaluation_tool/documents/sea_refactor 
 else
-    bokeh serve  --host $PUBLIC_IP:8888 --allow-websocket-origin $PUBLIC_IP:8888 --port 8888 /opt/model_evaluation_tool/documents/*
+    bokeh serve  --host $PUBLIC_IP:8888 --allow-websocket-origin $PUBLIC_IP:8888 --port 8888  /opt/model_evaluation_tool/documents/interaction /opt/model_evaluation_tool/documents/sea_refactor 
 fi
