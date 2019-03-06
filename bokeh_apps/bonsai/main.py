8a8d071272791c34ca8aa156bb62312d3e043a7c
andrew.ryan <andrew.g.m.ryan@gmail.com>
2019-03-06 13:04:22 +0000

    added a prototype minimalist Forest app called bonsai

---
+++ bokeh_apps/bonsai/main.py
@@ -0,0 +1,53 @@
+"""Minimalist implementation of FOREST"""
+import bokeh.plotting
+import bokeh.models
+import cartopy
+import numpy as np
+
+
+def main():
+    figure = full_screen_figure(
+        lon_range=(-20, 60),
+        lat_range=(-30, 20))
+    toolbar_box = bokeh.models.ToolbarBox(
+        toolbar=figure.toolbar,
+        toolbar_location="below")
+    document = bokeh.plotting.curdoc()
+    document.add_root(figure)
+    document.add_root(toolbar_box)
+
+
+def full_screen_figure(
+        lon_range=(-180, 180),
+        lat_range=(-80, 80)):
+    x_range, y_range = transform(
+        lon_range,
+        lat_range,
+        cartopy.crs.PlateCarree(),
+        cartopy.crs.Mercator.GOOGLE)
+    figure = bokeh.plotting.figure(
+        sizing_mode="stretch_both",
+        x_range=x_range,
+        y_range=y_range,
+        x_axis_type="mercator",
+        y_axis_type="mercator",
+        active_scroll="wheel_zoom")
+    figure.toolbar_location = None
+    figure.axis.visible = False
+    figure.min_border = 0
+    tile = bokeh.models.WMTSTileSource(
+        url='https://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png',
+        attribution="&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+    )
+    figure.add_tile(tile)
+    return figure
+
+
+def transform(x, y, src_crs, dst_crs):
+    x, y = np.asarray(x), np.asarray(y)
+    xt, yt, _ = dst_crs.transform_points(src_crs, x.flatten(), y.flatten()).T
+    return xt, yt
+
+
+if __name__.startswith("bk"):
+    main()
