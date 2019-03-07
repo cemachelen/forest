4b97d3553065ec3733db4da9d47ba99c5cfd8b20
andrew.ryan <andrew.g.m.ryan@gmail.com>
2019-03-07 10:42:56 +0000

    added title and state object to keep track of menu selections

--- bokeh_apps/bonsai/main.py
+++ bokeh_apps/bonsai/main.py
@@ -1,5 +1,6 @@
 """Minimalist implementation of FOREST"""
 import os
+import glob
 import yaml
 import bokeh.plotting
 import bokeh.models
@@ -12,13 +13,22 @@ class Config(object):
                  title="Bonsai - miniature Forest",
                  lon_range=None,
                  lat_range=None,
-                 models=None):
+                 models=None,
+                 model_dir=None):
         def assign(value, default):
             return default if value is None else value
         self.title = title
         self.lon_range = assign(lon_range, [-180, 180])
         self.lat_range = assign(lat_range, [-80, 80])
         self.models = assign(models, [])
+        self.model_dir = model_dir
+
+    @classmethod
+    def merge(cls, *dicts):
+        settings = {}
+        for d in dicts:
+            settings.update(d)
+        return Config(**settings)
 
     @property
     def model_names(self):
@@ -31,9 +41,13 @@ class Config(object):
 
     @classmethod
     def load(cls, path):
+        return cls(**cls.load_dict(path))
+
+    @staticmethod
+    def load_dict(path):
         with open(path) as stream:
             kwargs = yaml.load(stream)
-        return cls(**kwargs)
+        return kwargs
 
 
 class Environment(object):
@@ -43,15 +57,18 @@ class Environment(object):
 
 def parse_env():
     config_file = os.environ.get("FOREST_CONFIG", None)
-    return Environment(config_file=config_file)
+    model_dir = os.environ.get("FOREST_MODEL_DIR", None)
+    return Environment(
+        config_file=config_file,
+        model_dir=model_dir)
 
 
 def main():
     env = parse_env()
-    if env.config_file is None:
-        config = Config()
-    else:
-        config = Config.load(env.config_file)
+    dicts = [dict(model_dir=env.model_dir)]
+    if env.config_file is not None:
+        dicts.append(Config.load_dict(env.config_file))
+    config = Config.merge(*dicts)
 
     figure = full_screen_figure(
         lon_range=config.lon_range,
@@ -65,25 +82,114 @@ def main():
             dropdown.label = value
         return on_click
 
-    def on_model(config):
-        def on_click(value):
-            print(value, config.model_pattern(value))
-        return on_click
-
     menu = [(name, name) for name in config.model_names]
     dropdown = bokeh.models.Dropdown(
         label="Select model",
         menu=menu)
     dropdown.on_click(select(dropdown))
-    dropdown.on_click(on_model(config))
+
+    state = State()
+    dropdown.on_click(state.on("model"))
+
+    list_files = ListFiles(config)
+    state.register(list_files)
+
+    date_picker = bokeh.models.DatePicker()
+    date_picker.on_change("value", state.on_change("date"))
+
+    bonsai_title = Title(figure)
+    state.register(bonsai_title)
 
     document = bokeh.plotting.curdoc()
     document.add_root(figure)
     document.add_root(toolbar_box)
     document.add_root(bokeh.layouts.column(
+        date_picker,
         dropdown,
         name="controls"))
     document.title = config.title
+
+
+class Title(object):
+    def __init__(self, figure):
+        self.caption = bokeh.models.Label(
+            x=0,
+            y=0,
+            x_units="screen",
+            y_units="screen",
+            y_offset=-10,
+            text_font_size="12pt",
+            text_align="center",
+            text_baseline="top",
+            text="")
+        figure.add_layout(self.caption)
+        custom_js = bokeh.models.CustomJS(
+            args=dict(caption=self.caption), code="""
+                caption.x = cb_obj.layout_width / 2
+                caption.y = cb_obj.layout_height
+            """)
+        figure.js_on_change("layout_width", custom_js)
+        figure.js_on_change("layout_height", custom_js)
+
+    def notify(self, state):
+        words = []
+        if "model" in state:
+            words.append(state["model"])
+        if "date" in state:
+            words.append("{:%Y-%m-%d %H:%M}".format(state["date"]))
+        text = " ".join(words)
+        self.render(text)
+
+    def render(self, text):
+        self.caption.text = text
+
+
+def on_model(config):
+    def on_click(value):
+        print(value)
+        if config.model_dir is None:
+            pattern = config.model_pattern(value)
+        else:
+            pattern = os.path.join(
+                config.model_dir,
+                config.model_pattern(value))
+        for path in sorted(glob.glob(pattern)):
+            print(path)
+    return on_click
+
+
+class ListFiles(object):
+    def __init__(self, config):
+        self.config = config
+
+    def notify(self, state):
+        print(state)
+
+
+class State(object):
+    def __init__(self):
+        self.state = {}
+        self.subscribers = []
+
+    def on(self, attr):
+        def callback(value):
+            self.announce(attr, value)
+        return callback
+
+    def on_change(self, state_attr):
+        def callback(attr, old, new):
+            self.announce(state_attr, new)
+        return callback
+
+    def register(self, subscriber):
+        self.subscribers.append(subscriber)
+
+    def announce(self, attr, value):
+        self.state = dict(self.state)
+        self.state[attr] = value
+        for s in self.subscribers:
+            s.notify(self.state)
+
 
 
 def full_screen_figure(
--- bokeh_apps/bonsai/test_main.py
+++ bokeh_apps/bonsai/test_main.py
@@ -1,22 +1,53 @@
 # pylint: disable=missing-docstring, invalid-name
 import unittest
+import unittest.mock
 import os
 import yaml
 import main
 
 
+class TestEnvironment(unittest.TestCase):
+    def tearDown(self):
+        for variable in ["FOREST_MODEL_DIR"]:
+            if variable in os.environ:
+                del os.environ[variable]
+
+    def test_parse_env_given_forest_model_dir(self):
+        os.environ["FOREST_MODEL_DIR"] = "/some/dir"
+        result = main.parse_env().model_dir
+        expect = "/some/dir"
+        self.assertEqual(expect, result)
+
+    def test_parse_env_default_forest_model_dir(self):
+        result = main.parse_env().model_dir
+        expect = None
+        self.assertEqual(expect, result)
+
+
 class TestConfig(unittest.TestCase):
     def setUp(self):
         self.path = "test-config.yaml"
+        self.config = main.Config()
 
     def tearDown(self):
         if os.path.exists(self.path):
             os.remove(self.path)
 
+    def test_merge_configs(self):
+        dict_0 = dict(
+            lat_range=[-5, 5],
+            lon_range=[-180, 180])
+        with open(self.path, "w") as stream:
+            yaml.dump({"lon_range": [-10, 10]}, stream)
+            dict_1 = main.Config.load_dict(self.path)
+        merged = main.Config.merge(dict_0, dict_1)
+        self.assertEqual(merged.lon_range, [-10, 10])
+        self.assertEqual(merged.lat_range, [-5, 5])
+
     def test_load(self):
         with open(self.path, "w") as stream:
             yaml.dump({"lon_range": [-10, 10]}, stream)
-        result = main.Config.load(self.path).lon_range
+        result = self.config.load(self.path).lon_range
         expect = [-10, 10]
         self.assertEqual(expect, result)
 
@@ -41,6 +72,12 @@ class TestConfig(unittest.TestCase):
         expect = "a.nc"
         self.assertEqual(expect, result)
 
+    def test_model_dir(self):
+        self.check_kwarg("model_dir", "/some/dir")
+
+    def test_default_model_dir_returns_none(self):
+        self.check_default("model_dir", None)
+
     def test_default_lon_range(self):
         self.check_default("lon_range", [-180, 180])
 
@@ -56,3 +93,30 @@ class TestConfig(unittest.TestCase):
     def check_default(self, attr, expect):
         result = getattr(main.Config(), attr)
         self.assertEqual(expect, result)
+
+    def check_kwarg(self, attr, expect):
+        result = getattr(main.Config(**{attr: expect}), attr)
+        self.assertEqual(expect, result)
+
+
+class TestOnClick(unittest.TestCase):
+    def test_on_click(self):
+        state = main.State()
+        view = unittest.mock.Mock()
+        state.register(view)
+        state.on("model")("A")
+        view.notify.assert_called_once_with({"model": "A"})
+
+    def test_state_merges_streams(self):
+        state = main.State()
+        view = unittest.mock.Mock()
+        state.register(view)
+        state.on("model")("A")
+        state.on("date")("B")
+        state.on("model")("B")
+        calls = [
+            unittest.mock.call({"model": "A"}),
+            unittest.mock.call({"model": "A", "date": "B"}),
+            unittest.mock.call({"model": "B", "date": "B"}),
+        ]
+        view.notify.assert_has_calls(calls)
